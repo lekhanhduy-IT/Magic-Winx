0f8abdfd1b6696ec8639845d6270c7630a939f1f
lekhanhduy-IT <khanhduyhazo@gmail.com>
2025-05-02 07:17:08 +0700

    Add files via upload

--- win.sql
+++ win.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1:3406
--- Thời gian đã tạo: Th5 01, 2025 lúc 06:01 PM
+-- Thời gian đã tạo: Th5 02, 2025 lúc 02:15 AM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.1.25
 
@@ -67,23 +67,24 @@ CREATE TABLE `outfits` (
   `effect3` text NOT NULL,
   `status_effect1` tinyint(4) NOT NULL,
   `status_effect2` tinyint(4) NOT NULL,
-  `status_effect3` tinyint(4) NOT NULL
+  `status_effect3` tinyint(4) NOT NULL,
+  `PH` int(11) NOT NULL DEFAULT 10000
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `outfits`
 --
 
-INSERT INTO `outfits` (`id`, `user_id`, `image1`, `image2`, `image3`, `image4`, `updated_at`, `x`, `y`, `effect`, `status_effect`, `effect1`, `effect2`, `effect3`, `status_effect1`, `status_effect2`, `status_effect3`) VALUES
-(3, 3, 'user_static.png', 'user_fly.png', 'win44.png', 'win4.png', '2025-04-28 04:17:16', 156, 151, '', 0, '', '', '', 0, 0, 0),
-(4, 4, 'user_fly.png', 'user_static.png', 'win3_bloomix.png', 'win33.png', '2025-04-25 19:07:07', 100, 100, '', 0, '', '', '', 0, 0, 0),
-(5, 5, 'static.png', 'fly.png', 'image/canh/win9.png', 'image/canh/win99.png', '2025-05-01 13:42:59', 869, 51, 'effect.gif', 0, '', '', '', 0, 0, 0),
-(6, 6, 'image/user/stella_static.png', 'image/user/stella_fly.png', 'image/canh/win11.png', 'image/canh/win1.png', '2025-05-01 13:44:36', 336, 268, '', 0, '', '', 'image/effect/effect.gif', 0, 0, 1),
-(7, 7, 'image/user/musa_static.png', 'image/user/musa_fly.png', 'image/canh/win1515.png', 'image/canh/win15.png', '2025-05-01 15:21:12', 653, 400, 'image/effect/effect6.gif', 1, '', '', '', 0, 0, 0),
-(8, 8, 'image/user/tech_static.png', 'image/user/tech_fly.png', 'image/canh/win44.png', 'image/canh/win4.png', '2025-05-01 13:45:07', 474, 419, 'image/effect/effect4.gif', 1, '', '', '', 0, 0, 0),
-(9, 9, 'image/user/fly.png', 'image/user/static.png', 'image/canh/win66.png', 'image/canh/win6.png', '2025-05-01 13:45:16', 877, 362, 'image/effect/chan_effect.gif', 1, '', '', '', 0, 0, 0),
-(12, 12, 'image/user/pixi1_static.png', 'image/user/pixi1_fly.png', 'image/canh/win1414.png', 'image/canh/win14.png', '2025-05-01 13:45:58', 575, 387, 'image/effect/effect7.gif', 1, 'image/effect/chan_effect.gif', 'image/effect/effect8.gif', 'image/effect/effect9.gif', 0, 0, 0),
-(13, 13, 'image/user/pixi1_static.png', 'image/user/pixi1_fly.png', 'image/canh/win1515.png', 'image/canh/win15.png', '2025-05-01 15:34:31', 590, 347, '', 0, '', '', '', 0, 0, 0);
+INSERT INTO `outfits` (`id`, `user_id`, `image1`, `image2`, `image3`, `image4`, `updated_at`, `x`, `y`, `effect`, `status_effect`, `effect1`, `effect2`, `effect3`, `status_effect1`, `status_effect2`, `status_effect3`, `PH`) VALUES
+(3, 3, 'user_static.png', 'user_fly.png', 'win44.png', 'win4.png', '2025-04-28 04:17:16', 156, 151, '', 0, '', '', '', 0, 0, 0, 10000),
+(4, 4, 'user_fly.png', 'user_static.png', 'win3_bloomix.png', 'win33.png', '2025-04-25 19:07:07', 100, 100, '', 0, '', '', '', 0, 0, 0, 10000),
+(5, 5, 'static.png', 'fly.png', 'image/canh/win9.png', 'image/canh/win99.png', '2025-05-01 13:42:59', 869, 51, 'effect.gif', 0, '', '', '', 0, 0, 0, 10000),
+(6, 6, 'image/user/stella_static.png', 'image/user/stella_fly.png', 'image/canh/win11.png', 'image/canh/win1.png', '2025-05-01 13:44:36', 336, 268, '', 0, '', '', 'image/effect/effect.gif', 0, 0, 1, 10000),
+(7, 7, 'image/user/musa_static.png', 'image/user/musa_fly.png', 'image/canh/win1515.png', 'image/canh/win15.png', '2025-05-01 15:21:12', 653, 400, 'image/effect/effect6.gif', 1, '', '', '', 0, 0, 0, 10000),
+(8, 8, 'image/user/tech_static.png', 'image/user/tech_fly.png', 'image/canh/win44.png', 'image/canh/win4.png', '2025-05-01 13:45:07', 474, 419, 'image/effect/effect4.gif', 1, '', '', '', 0, 0, 0, 10000),
+(9, 9, 'image/user/fly.png', 'image/user/static.png', 'image/canh/win66.png', 'image/canh/win6.png', '2025-05-01 19:28:53', 346, 257, 'image/effect/chan_effect.gif', 1, '', '', '', 0, 0, 0, 0),
+(12, 12, 'image/user/pixi1_static.png', 'image/user/pixi1_fly.png', 'image/canh/win1414.png', 'image/canh/win14.png', '2025-05-01 19:26:50', 206, 323, 'image/effect/effect7.gif', 1, 'image/effect/chan_effect.gif', 'image/effect/effect8.gif', 'image/effect/effect9.gif', 0, 0, 0, 10000),
+(13, 13, 'image/user/pixi1_static.png', 'image/user/pixi1_fly.png', 'image/canh/win1515.png', 'image/canh/win15.png', '2025-05-01 18:14:40', 762, 237, '', 0, '', '', '', 0, 0, 0, 10000);
 
 -- --------------------------------------------------------
 
@@ -111,8 +112,8 @@ INSERT INTO `users` (`id`, `username`, `password`, `created_at`, `status`) VALUE
 (7, 'nhu2k25', '$2y$10$dZry.6RUctqUcWYetPMyQeDLdSklO.jU4yDwBgsbFLyuWDnwKiXLW', '2025-04-27 01:02:02', 0),
 (8, 'nghi2k25', '$2y$10$UhtPbbi1bi82ORmsCF0wROEVthxHMtjQ7TIcrTousBWpMchMyHOT2', '2025-04-27 01:04:52', 1),
 (9, 'ly2k25', '$2y$10$15JjfOlOfu9sNuFxMvRFL.ijBVP5MrwKOhuMTEoq/8GPh9dp0uMKK', '2025-04-27 01:06:28', 1),
-(12, 'pixi1', '$2y$10$8ELQ/1UnXC2gTA0R0LMIOe.lXFnNZb9Ttu.ExpbEqQBpdlBezcuBa', '2025-04-29 17:15:15', 0),
-(13, 'toan2k25', '$2y$10$UjaYXwyPdEgkrf0Fd4Re3uOpTqrvKDqKpVSOW14uXGCYgotfxIEN.', '2025-05-01 15:25:32', 1);
+(12, 'pixi1', '$2y$10$8ELQ/1UnXC2gTA0R0LMIOe.lXFnNZb9Ttu.ExpbEqQBpdlBezcuBa', '2025-04-29 17:15:15', 1),
+(13, 'toan2k25', '$2y$10$UjaYXwyPdEgkrf0Fd4Re3uOpTqrvKDqKpVSOW14uXGCYgotfxIEN.', '2025-05-01 15:25:32', 0);
 
 --
 -- Chỉ mục cho các bảng đã đổ
